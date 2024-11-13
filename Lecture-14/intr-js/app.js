394d629fdb6ba2adcb89f68c3369b827685082b6
Samarth <officialdevelopersamarth@gmail.com>
2024-11-13 12:46:33 +0530

    lecture 14 codes

---
+++ Lecture-14/intr-js/app.js
@@ -0,0 +1,73 @@
+// let abc= 10;
+// console.log(abc);
+
+// -----------------------
+
+// numbers
+// let a = 10;
+// let b = 10.15;
+// let c = +Infinity;
+// let d = -Infinity;
+// console.log(a);
+// console.log(b);
+// console.log(c);
+// console.log(d);
+// console.log(a, b , c , d);
+
+// -----------------------
+
+// console.log( typeof(a) );
+// console.log( typeof(b) );
+// console.log( typeof(c) );
+// console.log( typeof(d) );
+
+
+// --------------------------------------------
+
+
+// string -> 3
+
+// let a = 'sam'
+// let b = "vohra"
+// let c = `maverick`  //(backticks)
+
+// console.log(a , b ,  c);
+
+// console.log(typeof(a));
+// console.log(typeof(b));
+// console.log(typeof(c));
+
+
+// -------------------------------
+
+// let naam = "samarth";
+// let kaam = "time paas";
+// let state = "new delhi";
+
+// hi i am samarth my job is to do time paas and i put up in new delhi
+
+// concatination
+
+// let out = 'hi i am ' + naam + " my job is to do " + kaam + ' and i put up in ' + state
+
+// console.log(out);
+
+// string template literals
+// let out2 = `hi i am ${naam} my job is to do ${kaam} and i put up in ${state}`
+// console.log(out2);
+
+
+// ----------------------------
+
+// + , - , * , / , % , **
+
+// let a = 5;
+// let b = 2;
+
+// console.log(a/b); // 
+
+// console.log(a%b); // 
+
+
+
+
